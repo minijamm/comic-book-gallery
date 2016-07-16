03301811c692b5188fdbec0d5041837898e842fa
James Millichip <james@millichip.me>
2016-07-16 23:23:28 +0100

    Updated detail action method

--- src/comic-book-gallery/Controllers/ComicBooksController.cs
+++ src/comic-book-gallery/Controllers/ComicBooksController.cs
@@ -8,9 +8,17 @@ namespace comic_book_gallery.Controllers
 {
     public class ComicBooksController : Controller
     {
-        public string Detail()
+        public ActionResult Detail()
         {
-            return "Hello from the comic books controller";
+            if (DateTime.Today.DayOfWeek == DayOfWeek.Saturday)
+            {
+                return Redirect("/");
+            }
+            return Content("Hello from the comic books controller");
+
+
+
+
         }
     }
 }
