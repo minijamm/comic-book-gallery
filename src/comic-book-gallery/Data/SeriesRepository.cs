b914b91407598c143527e2531268e1f1ffc1be4f
James Millichip <james@millichip.me>
2016-07-23 22:57:46 +0100

    created series repository

---
+++ src/comic-book-gallery/Data/SeriesRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using comic_book_gallery.Models;
+
+namespace comic_book_gallery.Data
+{
+    public class SeriesRepository
+    {
+        public Series[] GetSeries()
+        {
+            return Data.Series;
+        }
+
+        public Series GetSeriesDetail(int id)
+        {
+
+            Series seriestoReturn = null;
+
+            foreach (var series in Data.Series)
+            {
+
+            }
+
+            return seriestoReturn;
+        }
+
+
+
+    }
+}
