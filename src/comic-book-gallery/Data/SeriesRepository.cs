3079273791a96549ea2f897af23e1632ea724d9f
James Millichip <james@millichip.me>
2016-07-25 22:15:48 +0100

    completed series repository

--- src/comic-book-gallery/Data/SeriesRepository.cs
+++ src/comic-book-gallery/Data/SeriesRepository.cs
@@ -21,6 +21,30 @@ namespace comic_book_gallery.Data
             foreach (var series in Data.Series)
             {
 
+                if (series.Id == id)
+                {
+                    seriestoReturn = series;
+                    break;
+                }
+            
+
+            }
+
+            if (seriestoReturn != null)
+            {
+                //return books for this series
+                var comicBooks = new ComicBook[0];
+
+                    foreach (var comicBook in Data.ComicBooks)
+                {
+                    if (comicBook.Series != null && comicBook.Series.Id == id)
+                    {
+                        Array.Resize(ref comicBooks, comicBooks.Length + 1);
+                        comicBooks[comicBooks.Length - 1] = comicBook;
+                    }
+
+
+                }
             }
 
             return seriestoReturn;
