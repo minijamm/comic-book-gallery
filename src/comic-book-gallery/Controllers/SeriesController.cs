68fabdb0502033c81815b65afc9f46d9bb620c2d
James Millichip <james@millichip.me>
2016-07-26 21:58:12 +0100

    "Added Series List view"

---
+++ src/comic-book-gallery/Controllers/SeriesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using comic_book_gallery.Data;
+using comic_book_gallery.Models;
+
+namespace comic_book_gallery.Controllers
+{
+    public class SeriesController : Controller
+    {
+
+        private SeriesRepository _seriesRepository = null;
+
+        public SeriesController()
+        {
+            _seriesRepository = new SeriesRepository();
+        }
+
+        public ActionResult Index()
+        {
+            var series = _seriesRepository.GetSeries();
+            return View(series);
+        }
+
+    }
+
+
+}
